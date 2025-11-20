1846ab1117f1ea390f716a5c70f5b32151733d33
mizukize <12221222mm@gmail.com>
2025-11-20 16:21:41 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,6 +6,6 @@ name = st.text_input("座右の銘なんですか")
 
 st.write(name)
 
-camera_photo = st.camera_imort("ハイチーズ！")
+camera_photo = st.camera_input("ハイチーズ！")
 if camera:
   st.image(camera, caption="写真",use_column_width=True)
